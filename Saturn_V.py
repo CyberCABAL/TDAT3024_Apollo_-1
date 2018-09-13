2f019a468cae5b682969bb8f282e94e6f19ff252
eivindug <eivind-gautvik@hotmail.com>
2018-09-13 11:03:26 +0200

    ordna data angående Saturn V sine steg

---
+++ Saturn_V.py
@@ -0,0 +1,29 @@
+totalmasse=2970*10**3
+
+#steg 1
+F1=35100*10**3
+tid1=168
+tørrvekt1=130*10**3
+totalvekt1=2290*10**3
+drivstoffvekt1=totalvekt1-tørrvekt1
+ms1=-(drivstoffvekt1/tid1)
+v1=F1/-ms1
+
+#steg 2
+F2=5141*10**3
+tid2=360
+tørrvekt2=40.1*10**3
+totalvekt2=496.2*10**3
+drivstoffvekt2=totalvekt2-tørrvekt2
+ms2=-(drivstoffvekt2/tid2)
+v2=F2/-ms2
+
+#steg 3
+F3=1000*10**3
+tid3=165+335 #2 burns
+tørrvekt3=13.5*10**3
+totalvekt3=123*10**3
+drivstoffvekt3=totalvekt3-tørrvekt3
+ms3=-(drivstoffvekt3/tid3)
+v3=F3/-ms3
+
